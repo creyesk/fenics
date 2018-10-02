edbc6427554e5a84015a48b01f0a6e265ced23ab
creyesk <creyesk@gmail.com>
2018-10-02 10:18:45 -0500

    added dirichlet boundary conditions

--- heat_equation1d.py
+++ heat_equation1d.py
@@ -9,26 +9,31 @@ class PeriodicBoundary(SubDomain):
    def map(self, x, y):
       y[0] = x[0] - 1.0
 
-T = 0.01
-num_steps = 10
+T = 10
+num_steps = 100
 dt = T / num_steps
 
-nx = 20000
+nx = 10000
 mesh = UnitIntervalMesh(nx)
-V = FunctionSpace(mesh, "CG", 2, constrained_domain=PeriodicBoundary())
+# V = FunctionSpace(mesh, "CG", 2, constrained_domain=PeriodicBoundary())
+V = FunctionSpace(mesh, 'P', 1)
+p1 = Expression('4*(x[0]-0.25)', degree=1)
+p2 = Expression('4*(0.75-x[0])', degree=1)
+u_0 = Expression('x[0] < 0.25 ? 0 : (x[0] < 0.5 ? p1 : (x[0] < 0.75 ? p2 : 0))',
+                 p1=p1, p2=p2, degree=1)
 
-# p1 = Expression('4*(x[0]-0.25)', degree=1)
-# p2 = Expression('4*(0.75-x[0])', degree=1)
-# u_0 = Expression('x[0] < 0.25 ? 0 : (x[0] < 0.5 ? p1 : (x[0] < 0.75 ? p2 : 0))',
-#                  p1=p1, p2=p2, degree=2)
-u_0 = Expression('exp(-pow(x[0] - 0.5, 2)*16) - exp(-pow(0.5, 2)*16)',
-                 degree=2)
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, Constant(0), boundary)
+# u_0 = Expression('exp(-pow(x[0] - 0.5, 2)*16) - exp(-pow(0.5, 2)*16)',
+#                  degree=2)
 
 # u_0 = Expression('x[0]<=0.5 ? 2*x[0] : 2 - 2*x[0]', degree=1)
 
 
-phi = Expression('sin(2*pi*(x[0]-0.25))', degree=2)
-# phi = Constant(1)
+phi = Expression('sin(2*pi*(x[0]-0.25)) + .01', degree=2)
+phi = Constant(1)
 u_n = interpolate(u_0, V)
 phi = interpolate(phi, V)
 
@@ -54,12 +59,9 @@ for n in range(num_steps):
     t += dt
 
     # Compute solution
-    solve(a == L, u)
+    solve(a == L, u, bc)
     
     vtkfile << u
-
-    # Plot solution
-    # plot(u)
 
     # Update previous solution
     u_n.assign(u)
